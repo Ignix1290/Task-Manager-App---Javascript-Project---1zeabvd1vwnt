4e7def7d319a4d1ee21fc79abe04d93c674cf95b
Ignix1290 <52908465+Ignix1290@users.noreply.github.com>
2023-02-12 19:15:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,45 @@
 let input=document.getElementById("new-items");
 let add=document.getElementById("btn");
 let tasks=document.getElementById("tasks");
+let modal=document.querySelector(".modal");
+let okBtn=document.getElementById("submit");
+let area=document.getElementById("area");
 
 add.addEventListener("click",()=>{
-    if(input.value===" "){
+    if(input.value===""){
         alert("Enter a task please!!!");
         return;
     }
     let open=document.querySelector("div[id=tasks]");
     open.innerHTML+=`<div draggable="true" ondragstart="dragStarted(event)" ondragend="dragEnded(event)"  class="content"><p>${input.value}<span></span></p>
-    <button type="button" onclick="describeTask(event)" class="describebtn"><i class="fa-solid fa-pen-to-square"></i></button>
+    <button type="button" onclick="describeTask(event)" class="describebtn fa-solid fa-pen-to-square"></button>
     <button type="button" onclick="deleteTask(event)" class="fa-solid fa-trash"></i></button>
     </br></br></div>`;
-    input.value=" ";
+    input.value="";
     
        
 });
 
 function describeTask(e){
     //console.log(e.target.parentElement);
-    let des=document.querySelectorAll("span");
-    for(let x of des){
-        x.innerHTML=`<textarea></textarea>`;
-    }
+    previousTarget=e.target.parentElement;
+    modal.classList.add("active");
 }
+
+okBtn.addEventListener("click", (e)=>{
+    //console.log(e.target.parentElement);
+    let spanEl=previousTarget.querySelector("div[class='content']>p>span");
+    spanEl.innerHTML=`<p class="description">${area.value}</p>`;
+    area.value="";
+    modal.classList.remove("active");
+    
+})
 
 function deleteTask(e) {
 	//console.log(e.target.parentElement);
 		e.target.parentElement.parentElement.removeChild(e.target.parentElement);
 }
 
-//let des=document.getElementsByClassName("describebtn");
-// des.addEventListener("click",()=>{
-// });
-//let del=document.getElementsByClassName("deletebtn");
-/*
-tasks.addEventListener("click", (e)=>{
-    if(e.target.classList.contains("deletebtn")){
-        e.target.parentElement.parentElement.remove();
-    }
- });
- */
 let el=document.getElementsByClassName("content");
 let dest=document.getElementsByClassName("destination");
 let draggedEl = null;
